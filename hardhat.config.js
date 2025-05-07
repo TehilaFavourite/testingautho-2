d3b4e0aa2f27a9a1e6be763bf2c20b5c113ff6a0
tehilafavourite <tehilafavourite@gmail.com>
2025-05-07 12:45:41 +0100

    first

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-foundry");
+require('@openzeppelin/hardhat-upgrades');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+};
